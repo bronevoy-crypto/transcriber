adb3e49be5c730cc1162576deb63f98a265eb91c
HRYNdev <vladimirshev10@gmail.com>
2026-03-31 09:05:00 +0500

    fix: force disable device_map for GigaAM e2e loading

--- transcribe/gigaam_e2e.py
+++ transcribe/gigaam_e2e.py
@@ -27,6 +27,7 @@ class GigaAME2ETranscriber(BaseTranscriber):
             revision=revision,
             trust_remote_code=True,
             low_cpu_mem_usage=False,
+            device_map=None,
         )
         self._model = wrapper.model
         logger.info("GigaAME2E: модель загружена")
